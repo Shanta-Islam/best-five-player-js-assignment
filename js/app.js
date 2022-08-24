af3c5b37697bd04b93ad0de99a98a738961480aa
Shanta-Islam <shantaislamshorna68@gmail.com>
2022-08-24 06:21:45 +0600

    select button and name with get element by id

---
+++ js/app.js
@@ -0,0 +1,44 @@
+document.getElementById("playerBtn1").addEventListener("click", function () {
+    const playerName = getPlayerName("playerName1");
+    setPlayerName(playerName, "playerBtn1");
+  })
+  
+  document.getElementById("playerBtn2").addEventListener("click", function () {
+    const playerName = getPlayerName("playerName2");
+    setPlayerName(playerName, "playerBtn2");
+  })
+  
+  document.getElementById("playerBtn3").addEventListener("click", function () {
+    const playerName = getPlayerName("playerName3");
+    setPlayerName(playerName, "playerBtn3");
+  })
+  
+  document.getElementById("playerBtn4").addEventListener("click", function () {
+    const playerName = getPlayerName("playerName4");
+    setPlayerName(playerName, "playerBtn4");
+  })
+  
+  document.getElementById("playerBtn5").addEventListener("click", function () {
+    const playerName = getPlayerName("playerName5");
+    setPlayerName(playerName, "playerBtn5");
+  })
+  
+  document.getElementById("playerBtn6").addEventListener("click", function () {
+    const playerName = getPlayerName("playerName6");
+    setPlayerName(playerName, "playerBtn6");
+  })
+  
+  document.getElementById("playerBtn7").addEventListener("click", function () {
+    const playerName = getPlayerName("playerName7");
+    setPlayerName(playerName, "playerBtn7");
+  })
+  
+  document.getElementById("playerBtn8").addEventListener("click", function () {
+    const playerName = getPlayerName("playerName8");
+    setPlayerName(playerName, "playerBtn8");
+  })
+  
+  document.getElementById("playerBtn9").addEventListener("click", function () {
+    const playerName = getPlayerName("playerName9");
+    setPlayerName(playerName, "playerBtn9");
+  })
